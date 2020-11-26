7a8c355e05dd23bd1ebb0a5c0af39db44b94f19d
Your Name <you@example.com>
2020-11-26 22:52:39 +0900

    changing.

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path(r'', views.base, name='base'),
-    path('ajax/', views.api, name='api'),
+    path("ajax/", views.api, name="api"),
     #path('', views.post_list, name='post_list'),
     #path('post/<int:pk>/', views.post_detail, name='post_detail'),
     #path('post/new/', views.post_new, name='post_new'),
