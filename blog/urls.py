b321a32c1a647ef1086566e06bfe96c34afc4b17
Your Name <you@example.com>
2020-11-26 22:50:01 +0900

    changing.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path('', views.base, name='base'),
-    path('api/', views.api, name="api"),
+    path(r'', views.base, name='base'),
+    path('ajax/', views.api, name='api'),
     #path('', views.post_list, name='post_list'),
     #path('post/<int:pk>/', views.post_detail, name='post_detail'),
     #path('post/new/', views.post_new, name='post_new'),
--- blog/views.py
+++ blog/views.py
@@ -17,9 +17,6 @@ def api(request):
 def base(request):
     return render(request, 'blog/base.html')
 
-def index(request):
-    return render(requests, 'index.html')
-
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
