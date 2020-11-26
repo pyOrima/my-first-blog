9d45e34f7be524e387f8a49d07a19415ee803099
Your Name <you@example.com>
2020-11-26 10:03:10 +0900

    changing.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from . import views
-
+import sys
+sys.path.append('/home/pyOrima/pyorima.pythonanywhere.com')
+from blog import views
 
 urlpatterns = [
+    path('', views.base, name='base'),
+    path('api/', views.api, name="api"),
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
