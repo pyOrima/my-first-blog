87c09d9397d2ce42bdf6d5b936dabeef09ef150c
Your Name <you@example.com>
2020-11-26 11:09:01 +0900

    changing.

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'blog'
-
 urlpatterns = [
     path('', views.base, name='base'),
     path('api/', views.api, name="api"),
-    path('get/api/', views.api, name='api'),
     #path('', views.post_list, name='post_list'),
     #path('post/<int:pk>/', views.post_detail, name='post_detail'),
     #path('post/new/', views.post_new, name='post_new'),
